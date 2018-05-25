412f7c442b42c6ed172421e5129bbb9a495168dd
Stefan Misic <stefan.misic.491@gmail.com>
2018-05-25 09:17:05 +0200

    Add React and Webpack

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './App.css';
+import { HomeContainer } from './features/home'
+
+const App = () => (
+  <div className="App">
+    <HomeContainer />
+  </div>
+);
+
+export default App;
---
+++ src/features/home/containers/HomeContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HomeContainer = () => (
+  <div>
+    <h1>Hello, React!</h1>
+  </div>
+);
+
+export default HomeContainer;
